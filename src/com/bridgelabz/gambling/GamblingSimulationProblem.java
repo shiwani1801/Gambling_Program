04970ee3c53db66ddeeb2e58abe3bc362877ece2
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 12:22:22 -0800

    Added:check gambler win or lost the best

--- src/com/bridgelabz/gambling/GamblingSimulationProblem.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.gambling;
-
+import java.util.Random;
 public class GamblingSimulationProblem {
     //Default Constructor
     public GamblingSimulationProblem() {
@@ -7,7 +7,6 @@ public class GamblingSimulationProblem {
         System.out.println();
         System.out.println("Gambler starts the game...");
     }
-
     //Declaring constants
     final int betMoney = 1;
     final int stakePerDay = 100;
@@ -17,13 +16,33 @@ public class GamblingSimulationProblem {
         //Initialize the object.
         GamblingSimulationProblem gambling = new GamblingSimulationProblem();
         System.out.println();
-
-        //Declaring variables
-        int stake = 0;
-        stake = stake + gambling.stakePerDay;
         System.out.println("Initial stake of Gambler is : "+gambling.stakePerDay);
         System.out.println("Initial betting money in gambling is : "+gambling.betMoney);
+        startsGambling(gambling);
+    }
 
-        System.out.println("stake="+stake);
+    //Method for starting the game.
+    public static void startsGambling(GamblingSimulationProblem gambling) {
+        //Declaring variables
+        Random random = new Random();
+        int stake = 0;
+        stake = stake + gambling.stakePerDay;
+        int bet = random.nextInt(2);
+
+        //Checking randomly if win or loose.
+        switch (bet)
+        {
+            case 1 :
+                System.out.println("Gambler won the bet.");
+                stake = stake + gambling.betMoney;
+                break;
+            default :
+                System.out.println("Gambler lost the bet.");
+                stake = stake - gambling.betMoney;
+                break;
+        }
+
+        System.out.println("Amount remaining at Gambler is : "+stake);
+
     }
 }
