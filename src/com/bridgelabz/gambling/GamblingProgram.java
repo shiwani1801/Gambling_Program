936be3ebeb496d9b2fd1ed0a35e94089b01308c4
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 12:09:58 -0800

    Added:Gambling Simulation Program

--- src/com/bridgelabz/gambling/GamblingProgram.java
+++ src/com/bridgelabz/gambling/GamblingProgram.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.gambling;
 
-public class GamblingSimulationProblem {
+public class GamblingProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
     }
