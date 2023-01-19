7ef692e18323a92d5ed0324abccaf7502bb1f223
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 12:32:33 -0800

    Added:know Total amount win or lost by gambler in 20 days

--- src/com/bridgelabz/gambling/GamblingSimulationProblem.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -10,17 +10,19 @@ public class GamblingSimulationProblem {
     }
 
     //Declaring constants
-    final int betMoney = 1;
-    final int stakePerDay = 100;
-    final double percentage = 0.5;   //as it is given 50%
+    private final static int betMoney = 1;
+    private final static int stakePerDay = 100;
+    private final static double percentage = 0.5;   //as it is given 50%
+    private final static int maxDays = 20;
 
     public static void main(String[] args) {
 
         //Initialize the object.
         GamblingSimulationProblem gambling = new GamblingSimulationProblem();
         System.out.println();
-        System.out.println("Initial stake of Gambler is : "+gambling.stakePerDay);
-        System.out.println("Initial betting money in gambling is : "+gambling.betMoney);
+        System.out.println("Initial stake of Gambler is : " + stakePerDay);
+        System.out.println("Initial betting money in gambling is : " + betMoney);
+        System.out.println();
         startsGambling(gambling);
 
     }
@@ -29,35 +31,59 @@ public class GamblingSimulationProblem {
     public static void startsGambling(GamblingSimulationProblem gambling) {
         //Declaring variables
         Random random = new Random();
-        int totalStake = 0;
-        totalStake = totalStake + gambling.stakePerDay;
-        System.out.println(gambling.percentage);
-        int highestStake = (int) (totalStake+(totalStake * gambling.percentage));
-        int lowestStake = (int) (totalStake-(totalStake * gambling.percentage));
+        int highestStake = (int) (stakePerDay + (stakePerDay * percentage));
+        int lowestStake = (int) (stakePerDay - (stakePerDay * percentage));
 
-        //Checking randomly if win or loose with the condition.
-        totalStake = checkingCondition(gambling,random,totalStake,highestStake,lowestStake);
-        System.out.println("Gambler resigns for the day.");
-        System.out.println("Amount remaining at Gambler is : "+totalStake);
-
+        //Checking randomly if win or loose with the condition for 20 days
+        checkingCondition(highestStake, lowestStake, random);
     }
 
-    public static int checkingCondition(GamblingSimulationProblem gambling, Random random, int totalStake, int highestStake, int lowestStake) {
-        while (totalStake != highestStake && totalStake != lowestStake)
-        {
-            int bet = random.nextInt(2);
-            switch (bet) {
-                case 1:
-                    System.out.println("Gambler won the bet.");
-                    totalStake = totalStake + gambling.betMoney;
-                    break;
-                default:
-                    System.out.println("Gambler lost the bet.");
-                    totalStake = totalStake - gambling.betMoney;
-                    break;
+    public static void checkingCondition(int highestStake, int lowestStake, Random random) {
+        int totalWon = 0, totalLost = 0, totalDollars = 0, maxProfit = 0, maxLoss = 0, maxCount;
+        for (int i = 1; i <= maxDays; i++) {
+            int totalStake = stakePerDay;
+            while (totalStake != highestStake && totalStake != lowestStake) {
+                int bet = random.nextInt(2);
+                switch (bet) {
+                    case 1:
+                        //Gambler Won
+                        totalStake = totalStake + betMoney;
+                        totalDollars = highestStake - stakePerDay;
+                        maxProfit++;
+                        break;
+                    default:
+                        //Gambler Lost
+                        totalStake = totalStake - betMoney;
+                        totalDollars = stakePerDay - lowestStake;
+                        maxLoss++;
+                        break;
+                }
             }
+            System.out.println("Gambler resigns for the day " + i);
+            System.out.println("Amount remaining at Gambler is : " + totalStake);
+            System.out.println("Amount won count by Gambler : " + maxProfit + " for day " + i);
+            System.out.println("Amount lost count by Gambler : " + maxLoss + " for day " + i);
+            if (totalStake == highestStake) {
+                maxCount = maxProfit - maxLoss;
+                System.out.println("Gambler having highest won count : " + maxCount + " for the day " + i);
+                System.out.println("Gambler won by $" + totalDollars + " for the day " + i);
+                totalWon = totalWon + totalDollars;
+            } else if (totalStake == lowestStake) {
+                maxCount = maxLoss - maxProfit;
+                System.out.println("Gambler having highest lost count : " + maxCount + " for the day " + i);
+                System.out.println("Gambler lost by $" + totalDollars + " for the day " + i);
+                totalLost = totalLost + totalDollars;
+            }
+            System.out.println();
         }
-        return totalStake;
-
+        if (totalWon > totalLost) {
+            int total = totalWon - totalLost;
+            System.out.println("Gambler having overall total won $" + total + " in this 20 days");
+        } else if (totalLost > totalWon) {
+            int total = totalLost - totalWon;
+            System.out.println("Gambler having overall total lost $" + total + " in this 20 days");
+        } else if (totalWon == totalLost) {
+            System.out.println("Gambler neither having total won or total lost in this 20 days");
+        }
     }
 }
