d800035970bee4992a20c7ea759cee7128ee7462
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-18 23:35:05 -0800

    Update GamblingProgram.java

--- src/com/bridgelabz/gambling/GamblingProgram.java
+++ src/com/bridgelabz/gambling/GamblingProgram.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.gambling;
 
-public class GamblingProgram {
+public class GamblingSimulationProblem  {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
     }
